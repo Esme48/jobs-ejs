73545580ffa547ebb47ec09fea31a0b0f71ab9e3
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-06-17 14:26:48 -0700

    Added Authentiication component

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use("/secretWord", auth, secretWordRouter);
 app.use("/secretWord", secretWordRouter);
 
 const jobs = require("./routes/jobs");
-app.use("/jobs", jobs);
+app.use("/jobs", auth, jobs);
 
 app.use((req, res) => {
   res.status(404).send(`That page (${req.url}) was not found.`);
